e306a850c177439281157243e1371759f2e0f3b3
AlvarezGui <guilherme_alvarez18@hotmail.com>
2022-08-09 21:49:32 -0300

    AT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'ods-detalhe',
+    loadChildren: () => import('./ods-detalhe/ods-detalhe.module').then( m => m.OdsDetalhePageModule)
+  },
 ];
 
 @NgModule({
---
+++ src/app/models/iods.ts
@@ -0,0 +1,8 @@
+export interface Iods{
+  numero: string;
+  objetivo: string;
+  desc: string;
+  imagem: string;
+  pagina: string;
+  favorito: boolean;
+}
